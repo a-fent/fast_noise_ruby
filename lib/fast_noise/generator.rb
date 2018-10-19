922f157cbc8b4ec0286700660922efa28f5a2c79
Alex Fenton <alex@pressure.to>
2018-10-19 03:52:15 +0200

    Pre-defined Generator types

--- lib/fast_noise/generator.rb
+++ lib/fast_noise/generator.rb
@@ -12,4 +12,32 @@ module FastNoise
       self.noise_type = FastNoise::Perlin
     end
   end
+
+  class PerlinFractal < Generator
+    def initialize(args = {})
+      super()
+      self.noise_type = FastNoise::PerlinFractal
+    end
+  end
+  
+  class Simplex < Generator
+    def initialize(args = {})
+      super()
+      self.noise_type = FastNoise::Simplex
+    end
+  end
+  
+  class SimplexFractal < Generator
+    def initialize(args = {})
+      super()
+      self.noise_type = FastNoise::SimplexFractal
+    end
+  end
+  
+  class WhiteNoise < Generator
+    def initialize(args = {})
+      super()
+      self.noise_type = FastNoise::WhiteNoise
+    end
+  end
 end
